096a2d5cf015aa853db2000c920ad52bad624684
KristiyanSevov <kristiyan.sevov@gmail.com>
2018-07-08 18:31:05 +0300

    Added exam

--- 02. Defining Classes (Exercise)/src/Ex1_DefineClassPerson/Person.java
+++
@@ -1,6 +0,0 @@
-package Ex1_DefineClassPerson;
-
-public class Person {
-    private String name;
-    private int age;
-}
--- 04. Encapsulation (Exercise)/src/Ex5_PizzaCalories/Ingredient.java
+++
@@ -1,7 +0,0 @@
-package Ex5_PizzaCalories;
-
-public abstract class Ingredient {
-    protected static final double CALORIE_BASE = 2;
-
-    public abstract double getCalories();
-}
